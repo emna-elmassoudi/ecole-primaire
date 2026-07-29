c5ce052c051852ef86bb990882fe6f53c5552046
emna-elmassoudi <emna.elmassoudi@polytechnicien.tn>
2026-07-29 17:31:38 +0100

    Fix: create instance and upload dirs on startup

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,11 @@
+import os
 from app import app, db
 from models import User
+
+os.makedirs(os.path.join(app.instance_path), exist_ok=True)
+
+upload_dir = os.path.join(app.root_path, 'static', 'uploads', 'home')
+os.makedirs(upload_dir, exist_ok=True)
 
 with app.app_context():
     db.create_all()
